528847653c72f923483aee4a3cbf2d44c0ef540c
Joussef Boulbane <joussef.boulbane@student.vives.be>
2026-05-19 13:15:57 +0200

    Connectie met mongoDb Succesvol

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+require('dotenv').config();
 const express = require('express'); 
-const app = express(); 
+const mongoose = require('mongoose');
+
+const app = express();
 
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Succesvol verbonden met MongoDB...'))
+    .catch(err => console.error('Kon geen verbinding maken met MongoDB:', err));
+
 app.get('/', (req, res) => {
-    res.send('Welcome to the marketplace '); 
+    res.send('Welcome to the marketplace'); 
 });
 
-const port = 3000;
-app.listen(port, () => console.log(`Server draait op poort ${port}...`)); 
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server draait op poort ${port}...`));
